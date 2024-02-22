9c6d3deb1794acf19406750187e8bef3f5f2b810
Ninette03 <n.agatesi@alustudent.com>
2024-02-22 06:09:21 -0800

    8 uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    print('{:02d}, '.format(num), end='')
-print('99')
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
