3da26bab8a094acf2d487a0042ed20a5bdf530a7
Ninette03 <n.agatesi@alustudent.com>
2024-03-05 01:34:20 -0800

    Define width and height

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Define a class Rectangle."""
+
+
+class Rectangle:
+    """Represent a rectangle with width and height."""
+
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__value = value
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__value = value
