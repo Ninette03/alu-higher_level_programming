164983f3163da34e8a9e4e82f4eefbd056bc05ae
Ninette03 <n.agatesi@alustudent.com>
2024-03-11 02:57:46 -0700

    task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Define an object attribute in lookup function"""
+
+
 def lookup(obj):
     """Return the list of available attributes and methods of an object"""
     return(dir(obj))
