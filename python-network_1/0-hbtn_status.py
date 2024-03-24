3bcede671888c6bce50bf78921e981e2edd9ce89
Ninette03 <n.agatesi@alustudent.com>
2024-03-24 04:57:52 -0700

    Task 9

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-""" Python script that fetches https://alu-intranet.hbtn.io/status """
+""" Pythonn script that fetches https://intranet.hbtn.io/status """
 import urllib.request
-import urllib.error
+
 
 if __name__ == "__main__":
-    url = 'https://alu-intranet.hbtn.io/status'
-    try:
-        with urllib.request.urlopen(url) as response:
-            html = response.read().decode("utf-8")
-            print('Body response:')
-            print('\t- type: {}'.format(type(html)))
-            print('\t- content: {}'.format(html))
-    except urllib.error.HTTPError as e:
-        print('Error code: {}'.format(e.code))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
