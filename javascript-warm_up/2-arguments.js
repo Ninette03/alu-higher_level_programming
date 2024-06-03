9bfb393dd1a815ca9d54aa93cde6e8684e1eea9e
Ninette03 <n.agatesi@alustudent.com>
2024-06-03 06:20:40 -0700

    Task 1

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const argnum = args.length;
+if (argnum == 0) {
+	console.log('No argument');
+}
+else if (argnum == 1) {
+	console.log ('Argument found');
+}
+else {
+	console.log('Arguments found');
+}
