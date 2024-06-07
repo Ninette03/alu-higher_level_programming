4098c86823babdff68684d1f5a651caa02df1b7f
Ninette03 <n.agatesi@alustudent.com>
2024-06-07 08:13:13 -0700

    Updated files

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,14 +3,14 @@
 const request = require('request');
 
 if (process.argv.length > 2) {
-    const url = process.argv[2];
-    request.get(url, (error, response) => {
-        if (error) {
-            console.error(`An error occurred while making the request: ${error}`);
-        } else {
-            console.log(`code: ${response.statusCode}`);
-        }
-    });
+  const url = process.argv[2];
+  request.get(url, (error, response) => {
+    if (error) {
+      console.error(`An error occurred while making the request: ${error}`);
+    } else {
+      console.log(`code: ${response.statusCode}`);
+    }
+  });
 } else {
-    console.log('provide the URL as an argument.');
+  console.log('provide the URL as an argument.');
 }
