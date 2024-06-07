0f314338caa6395cd833d2444a31f481c335249b
Ninette03 <n.agatesi@alustudent.com>
2024-06-07 08:00:58 -0700

    updated file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 
 function readFile (filePath) {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.error(`An error occurred while reading the file: ${err}`);
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+	      console.error(`An error occurred while reading the file: ${err}`);
         } else {
             console.log(data);
         }
